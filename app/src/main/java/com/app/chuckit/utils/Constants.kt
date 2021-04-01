98f06cf1c40d58e989ac618c14a6eb3cd7804ea1
Eduardo Martins <emartinslemos@gmail.com>
2021-04-01 19:27:00 -0300

    imp: tamanho de fonte em card de ChuckNorrisFact

--- app/src/main/java/com/app/chuckit/utils/Constants.kt
+++ app/src/main/java/com/app/chuckit/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.app.chuckit.utils
 
 object Constants {
     const val CHUCKIT_DATABASE_NAME = "CHUCKIT_DATABASE_NAME"
+    const val CHARACTER_LIMIT_TO_UPPERCASE = 80
+    const val TEXT_LARGE_SIZE = 18f
 }
