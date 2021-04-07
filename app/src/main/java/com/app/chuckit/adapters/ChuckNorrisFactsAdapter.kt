7bcc7df350fc0eb45df08abb91dc25000127ef42
Eduardo Martins <emartinslemos@gmail.com>
2021-04-07 00:48:12 -0300

    fix: tamanho da fonte em recyclerview

--- app/src/main/java/com/app/chuckit/adapters/ChuckNorrisFactsAdapter.kt
+++ app/src/main/java/com/app/chuckit/adapters/ChuckNorrisFactsAdapter.kt
@@ -1,14 +1,12 @@
 package com.app.chuckit.adapters
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.app.chuckit.databinding.ItemChuckNorrisFactBinding
 import com.app.chuckit.interfaces.ShareChuckNorrisFactClickListener
 import com.app.chuckit.models.ChuckNorrisFact
-import com.app.chuckit.utils.Constants.CHARACTER_LIMIT_TO_UPPERCASE
-import com.app.chuckit.utils.Constants.TEXT_LARGE_SIZE
+import com.app.chuckit.utils.ChuckNorrisFactHelper
 
 
 class ChuckNorrisFactsAdapter(
@@ -34,21 +32,25 @@ class ChuckNorrisFactsAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val chuckNorrisFact = chuckNorrisFacts[position]
         with(chuckNorrisFact) {
-            if (chuckNorrisFact.value.length <= CHARACTER_LIMIT_TO_UPPERCASE)
-                holder.itemBinding.textViewChuckNorrisFact.textSize = TEXT_LARGE_SIZE
+
+            holder.itemBinding.textViewChuckNorrisFact.textSize =
+                ChuckNorrisFactHelper.getTextSize(chuckNorrisFact.value.length)
+
             holder.itemBinding.textViewChuckNorrisFact.text = this.value
 
             holder.itemBinding.textViewChuckNorrisFactCategory.text =
                 formatCategories(chuckNorrisFact.categories)
 
             holder.itemBinding.imageViewShareChuckNorrisFact.setOnClickListener {
-                shareChuckNorrisFactClickListener.onShareChuckNorrisFactClickListener(chuckNorrisFact.url)
+                shareChuckNorrisFactClickListener.onShareChuckNorrisFactClickListener(
+                    chuckNorrisFact.url
+                )
             }
         }
     }
 
     private fun formatCategories(categories: List<String>): String {
-        if (categories.isEmpty()) return "#undefined"
+        if (categories.isEmpty()) return "#uncategorized"
 
         val stringBuilder = StringBuilder()
 
